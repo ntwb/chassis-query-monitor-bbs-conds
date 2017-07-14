2e7bc68fcfd392c2f58ab755bf35ad636a0e4731
Stephen Edgar <stephen@netweb.com.au>
2017-07-14 17:47:03 +1000

    https://youtu.be/5I2cYpMYDEU

--- modules/chassisquerymonitorbbsconds/manifests/init.pp
+++ modules/chassisquerymonitorbbsconds/manifests/init.pp
@@ -1,21 +1,21 @@
 # Install the Query Monitor bbPress & BuddyPress Conditionals plugin
 class chassisquerymonitorbbsconds (
-  $config,
-  $path = '/vagrant/extensions/chassis-query-monitor-bbs-conds'
+	$config,
+	$path = '/vagrant/extensions/chassis-query-monitor-bbs-conds'
 ) {
-  if ! ( File['/vagrant/content'] ) {
-    file { '/vagrant/content':
-      ensure => 'directory',
-    }
-  }
-  if ! ( File['/vagrant/content/plugins'] ) {
-    file { '/vagrant/content/plugins':
-      ensure => 'directory',
-    }
-  }
-  wp::plugin { 'query-monitor-bbpress-buddypress-conditionals':
-    ensure   => enabled,
-    location => '/vagrant/wp',
-    require  => Class['wp'],
-  }
+	if ! ( File['/vagrant/content'] ) {
+		file { '/vagrant/content':
+			ensure => 'directory',
+		}
+	}
+	if ! ( File['/vagrant/content/plugins'] ) {
+		file { '/vagrant/content/plugins':
+			ensure => 'directory',
+		}
+	}
+	wp::plugin { 'query-monitor-bbpress-buddypress-conditionals':
+		ensure   => enabled,
+		location => '/vagrant/wp',
+		require  => Class['wp'],
+	}
 }
